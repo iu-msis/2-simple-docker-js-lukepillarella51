08f6e2bc3c5c8f4eb307903f779494f008fddccc
Luke Pillarella <lpillarella.tmr@gmail.com>
2020-09-24 21:14:42 -0400

    Reload User Info Page

--- app/public/js/userinfo.app.js
+++ app/public/js/userinfo.app.js
@@ -18,23 +18,45 @@ var app = new Vue({
 
     formatDate(d) {
       return moment(d).format("MMM DD, YYYY");
-    }
+    },
+
+    handleGetUser: function(){
+      fetch('https://randomuser.me/api/')
+      .then( response => response.json() )
+      .then( json => {
+        var userList = json.results[0];
+        console.log(userList);
+
+        this.userName = userList.name.first + " " + userList.name.last;
+        this.userCountry = userList.location.country;
+        this.userDOB = userList.dob.date;
+        this.userAge = userList.dob.age;
+        this.userEmail = userList.email;
+        this.userImgLarge = userList.picture.large;
+        this.userImgThumb = userList.picture.thumbnail;
+
+    })
+
+    .catch((error) => {
+      console.error('Error:', error);
+      fetch('./dummy/randomuser.me-sample.json')
+      .then(response => response.json())
+      .then(json => {
+        var userList = json.results[0];
+        console.log(userList);
+        this.userName = userList.name.first + " " + userList.name.last;
+        this.userCountry = userList.location.country;
+        this.userDOB = userList.dob.date;
+        this.userAge = userList.dob.age;
+        this.userEmail = userList.email;
+        this.userImgLarge = userList.picture.large;
+        this.userImgThumb = userList.picture.thumbnail;
+      })
+    });
   },
-  created() {
-    fetch('https://randomuser.me/api/')
-    .then( response => response.json() )
-    .then( json => {
-      var userList = json.results[0];
-      console.log(userList);
+},
 
-      this.userName = userList.name.first + " " + userList.name.last;
-      this.userCountry = userList.location.country;
-      this.userDOB = userList.dob.date;
-      this.userAge = userList.dob.age;
-      this.userEmail = userList.email;
-      this.userImgLarge = userList.picture.large;
-      this.userImgThumb = userList.picture.thumbnail;
-
-  });
-}
+  created(){
+    this.handleGetUser();
+  }
 })
